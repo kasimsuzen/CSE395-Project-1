8c1073de3ce53d7d252c787654425e99177836c8
Ali <eksi.gyte.edu.tr>
2015-12-02 12:05:16 +0200

    Thread Added to Server

--- Server/network.h
+++ Server/network.h
@@ -4,8 +4,9 @@
 #define MAX_MSG 100
 #define MAX_LINE 100
 #define LINE_ARRAY_SIZE (MAX_MSG+1)
-#define SERVER_IP "46.101.113.151"
-#define SERVER_PORT 6667
+#define SERVER_IP "192.168.111.130"
+#define SERVER_PORT 8003
+#define THREAD_NUMBER 2
 
 int checkInternetAccess();
 
--- Server/server.cpp
+++ Server/server.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "network.h"
+#include <omp.h>
 
 using namespace std;
 
@@ -21,6 +22,7 @@ int main()
     struct sockaddr_in clientAddress, serverAddress;
     char line[LINE_ARRAY_SIZE];
     struct sigaction sa;
+    int deviceID=-1, interfaceID=-1;
     // Setup the sighub handler
     sa.sa_handler = &handle_signal;
 
@@ -68,18 +70,12 @@ int main()
              (struct sockaddr *) &serverAddress,
              sizeof(serverAddress)) < 0) {
         cerr << "cannot bind socket";
-        exit(1);
+        exit(1); 
     }
-    
-    // Wait for connections from clients.
-    // This is a non-blocking call; i.e., it registers this program with
-    // the system as expecting connections on this socket, and then
-    // this thread of execution continues on.
-    listen(listenSocket, 5);
     
     while (1) {
         cout << "Waiting for TCP connection on port " << SERVER_PORT << " ...\n";
-        
+    
         // Accept a connection with a client that is requesting one.  The
         // accept() call is a blocking call; i.e., this thread of
         // execution stops until a connection comes in.
@@ -87,46 +83,74 @@ int main()
         // separate from listenSocket.  We *could* accept more
         // connections on listenSocket, before connectSocket is closed,
         // but this program doesn't do that.
-        clientAddressLength = sizeof(clientAddress);
-        connectSocket = accept(listenSocket,
-                               (struct sockaddr *) &clientAddress,
-                               &clientAddressLength);
-        if (connectSocket < 0) {
-            cerr << "cannot accept connection ";
-            exit(1);
+
+        //Threading Part
+        //connectSocket private for each threads.
+        //deviceID and interfaceID used as shared.
+        #pragma omp parallel num_threads(THREAD_NUMBER) private(connectSocket) shared(deviceID, interfaceID)
+        {
+            // Wait for connections from clients.
+            // This is a non-blocking call; i.e., it registers this program with
+            // the system as expecting connections on this socket, and then
+            // this thread of execution continues on.
+            listen(listenSocket, 5);
+            
+            
+            clientAddressLength = sizeof(clientAddress);
+            connectSocket = accept(listenSocket,
+                                   (struct sockaddr *) &clientAddress,
+                                   &clientAddressLength);
+            if (connectSocket < 0) {
+                cerr << "cannot accept connection ";
+                exit(1);
+            }
+            
+            // Show the IP address of the client.
+            // inet_ntoa() converts an IP address from binary form to the
+            // standard "numbers and dots" notation.
+            cout << "  connected to " << inet_ntoa(clientAddress.sin_addr);
+            
+            // Show the client's port number.
+            // ntohs() converts a short int from network byte order (which is
+            // Most Significant Byte first) to host byte order (which on x86,
+            // for example, is Least Significant Byte first).
+            cout << ":" << ntohs(clientAddress.sin_port) << "\n";
+            
+            // Read lines from socket, using recv(), storing them in the line
+            // array.  If no messages are currently available, recv() blocks
+            // until one arrives.
+            // First set line to all zeroes, so we'll know where the end of
+            // the string is.
+            memset(line, 0x0, LINE_ARRAY_SIZE);
+
+            while (recv(connectSocket, line, MAX_MSG, 0) > 0) {
+                cout << "  --  " << line << "\n";
+
+                //Device sends data to server
+                if(strcmp(line, "Device") == 0)
+                {
+                    if(deviceID == -1)
+                        deviceID = connectSocket;
+
+                    if(interfaceID != -1)
+                        if (send(interfaceID, "DeviceRespond", strlen("DeviceRespond") + 1, 0) < 0)
+                            cerr << "Error: cannot send modified data";
+                }
+                //Interface sends data to server
+                else if(strcmp(line, "Interface") == 0)
+                {
+                    if(interfaceID == -1)
+                        interfaceID = connectSocket;
+
+                    if(deviceID != -1)
+                        if (send(deviceID, "InterfaceRespond", strlen("InterfaceRespond") + 1, 0) < 0)
+                            cerr << "Error: cannot send modified data";
+                }
+                
+                memset(line, 0x0, LINE_ARRAY_SIZE);  // set line to all zeroes
+            }
         }
         
-        // Show the IP address of the client.
-        // inet_ntoa() converts an IP address from binary form to the
-        // standard "numbers and dots" notation.
-        cout << "  connected to " << inet_ntoa(clientAddress.sin_addr);
-        
-        // Show the client's port number.
-        // ntohs() converts a short int from network byte order (which is
-        // Most Significant Byte first) to host byte order (which on x86,
-        // for example, is Least Significant Byte first).
-        cout << ":" << ntohs(clientAddress.sin_port) << "\n";
-        
-        // Read lines from socket, using recv(), storing them in the line
-        // array.  If no messages are currently available, recv() blocks
-        // until one arrives.
-        // First set line to all zeroes, so we'll know where the end of
-        // the string is.
-        memset(line, 0x0, LINE_ARRAY_SIZE);
-        while (recv(connectSocket, line, MAX_MSG, 0) > 0) {
-            cout << "  --  " << line << "\n";
-        
-            //server send message to client 
-            char xx[LINE_ARRAY_SIZE];
-            cout << "enter string" << endl;
-            cin >> xx;
-            
-            // Send converted line back to client.
-            if (send(connectSocket, xx, strlen(xx) + 1, 0) < 0)
-                cerr << "Error: cannot send modified data";
-            
-            memset(line, 0x0, LINE_ARRAY_SIZE);  // set line to all zeroes
-        }
     }
 }
 
