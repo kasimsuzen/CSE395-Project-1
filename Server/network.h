0483e296face5cea69fe75c58aca3adc6019d398
Kasım <kasmszen@gmail.com>
2015-12-18 10:24:47 +0200

    Server ip and ports updated device name macro defined

--- Server/network.h
+++ Server/network.h
@@ -4,9 +4,11 @@
 #define MAX_MSG 100
 #define MAX_LINE 100
 #define LINE_ARRAY_SIZE (MAX_MSG+1)
-#define SERVER_IP "46.101.113.151"
-#define SERVER_PORT 6667
+#define SERVER_IP "46.101.112.39"
+#define SERVER_PORT 8001
 #define THREAD_NUMBER 2
+#define DEVICE_NAME "Device"
+#define INTERFACE_NAME "Interface"
 
 int checkInternetAccess();
 void* clientCube(void *p);
